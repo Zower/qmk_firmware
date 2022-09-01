f1e9f87d1f2eff11c7f1b662dd73033cfa338924
Eirik Tobiassen <eirik@2park.no>
2022-09-01 15:03:48 +0200

    Smth

--- keyboards/wxsh/fang2/keymaps/shibaru/keymap.c
+++ keyboards/wxsh/fang2/keymaps/shibaru/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,     KC_5,  KC_A,                                        KC_C,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    NO_PLUS,
                  KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,     KC_B,  KC_B,                                        KC_D,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_P,    NO_ARNG,
         	     KC_BSPC,  KC_A,    KC_R,    KC_S,    KC_T,     KC_G,                                                        KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ESC,
-                 KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_D,     KC_V,     KC_MUTE,  KC_LGUI,               KC_INS,  XXXXXXX, KC_K,    KC_H,    KC_COMM, KC_DOT,  NO_MINS, NO_QUOT,
+                 KC_LCTRL, KC_Z,    KC_X,    KC_C,    KC_D,     KC_V,     KC_MUTE,  KC_LGUI,               KC_INS,  XXXXXXX, KC_K,    KC_H,    KC_COMM, KC_DOT,  NO_MINS, NO_QUOT,
                                              KC_LALT, KC_LCTRL, KC_SPACE, KC_ENT,   KC_LGUI,               XXXXXXX, KC_ENT,  LTAPR,   KC_DEL,  KC_PSCR
     ),
     [_LOWER] = LAYOUT(
