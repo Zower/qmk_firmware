466b2d8419cd3060c04de523a52ae1c188c2123c
Eirik Tobiassen <eirik@2park.no>
2022-09-06 18:41:28 +0200

    smth

--- keyboards/wxsh/fang2/keymaps/shibaru/keymap.c
+++ keyboards/wxsh/fang2/keymaps/shibaru/keymap.c
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_ESC,   KC_1,    KC_2,    KC_3,     KC_4,     KC_5,     XXXXXXX,                                  XXXXXXX, KC_6,    KC_7,     KC_8,    KC_9,    KC_0,     TG(_COLE),
                  KC_TAB,   KC_Q,    KC_W,    KC_F,     KC_P,     KC_B,     XXXXXXX,                                  XXXXXXX, KC_J,    KC_L,     KC_U,    KC_Y,    KC_SCLN,  XXXXXXX,
         	     KC_BSPC,  KC_A,    KC_R,    KC_S,     KC_T,     KC_G,                                                        KC_M,    KC_N,     KC_E,    KC_I,    KC_O,     KC_ESC,
-                 KC_LCTRL, KC_Z,    KC_X,    KC_C,     KC_D,     KC_V,     KC_MUTE, XXXXXXX,               XXXXXXX, KC_MUTE,  KC_K,    KC_H,     KC_COMM, KC_DOT,  KC_SLASH, TG(_GAMING),
-                                             KC_LGUI,  KC_LALT,  KC_TGLHR, XXXXXXX, XXXXXXX,               XXXXXXX, KC_LSFT,  KC_TGLS, KC_TGLSE, XXXXXXX 
+                 KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_D,     KC_V,     KC_MUTE, XXXXXXX,               XXXXXXX, KC_MUTE,  KC_K,    KC_H,     KC_COMM, KC_DOT,  KC_SLASH, TG(_GAMING),
+                                             KC_LGUI,  KC_LALT,  KC_TGLHR, KC_LCTRL, XXXXXXX,              XXXXXXX, XXXXXXX,  KC_TGLS, KC_TGLSE, XXXXXXX 
     ),
     [_SPEC] = LAYOUT(
                  _______, KC_F1,      KC_F2,    KC_F3,    KC_F4,     KC_F5,       KC_F11, 		                     KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  _______,
